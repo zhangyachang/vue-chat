d327a9e8caff9686f6e8ff663a8a73222f21a536
xiaojiaoya <937741304@qq.com>
2018-11-09 17:54:56 +0800

    增加axios

--- src/components/chatContent.vue
+++ src/components/chatContent.vue
@@ -2,8 +2,14 @@
   <div></div>
 </template>
 <script>
+  import axios from 'axios'
   export default {
-
+    created () {
+      axios.get(' http://xiaoye121.com:8000')
+        .then(res => {
+          console.log(res);
+        })
+    }
   }
 </script>
 <style>
